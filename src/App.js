b0acbb98b9ab1409e2cc914a21b0239d4ba87e43
PriontoAbdullah <priontosoft@gmail.com>
2020-10-10 11:43:04 +0600

    Initial Project Setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2> This is e-Learning Course Platform </h2>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
